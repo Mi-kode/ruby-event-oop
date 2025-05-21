a90e742b09c48096c594e8db70d145c41a43d85e
Tom Folan <tom@DESKTOP-3F0JOM0>
2025-05-21 17:54:53 +0200

    Update du calendrier

--- app.rb
+++ app.rb
@@ -11,8 +11,14 @@ require_relative 'lib/event'
 # Maintenant c'est open bar pour tester ton application. Tous les fichiers importants sont chargés
 # Tu peux faire User.new, Event.new, binding.pry, User.all, etc.
 
-event = Event.new("2019-01-13 09:00", 10, "standup quotidien")
+event = Event.new("2025-05-21 17:20", 10, "standup quotidien", ["truc@machin.com", "bidule@chose.fr"])
 puts "Événement de test :"
 puts "Date: #{event.start_date}"
 puts "Durée: #{event.duration} minutes"
+puts "Le rendez-vous fini à #{event.end_date}"
 puts "Titre: #{event.title}"
+puts "Participants: #{event.attendees}"
+puts "Le rendez-vous est-il passé ? #{event.is_past?}"
+puts "Le rendez-vous est-il à venir ? #{!event.is_past?}"
+puts "Le rendez-vous est-il pour bientôt ? #{event.is_soon?}"
+puts "Rendez-vous décalé au #{event.postpone_24h}"
--- lib/event.rb
+++ lib/event.rb
@@ -8,12 +8,33 @@ class Event
     attr_accessor :title
     attr_accessor :attendees
 
-    def initialize(start_date_to_save, duration_to_save, title_to_save = "")
+    def initialize(start_date_to_save, duration_to_save, title_to_save = "", attendees_to_save = [])
         @start_date = Time.parse(start_date_to_save)
         @duration = duration_to_save
         @title = title_to_save
-        # @attendees = attendees_to_save
+        @attendees = attendees_to_save
     end
+
+    def postpone_24h
+        postpone_date = @start_date + 86400
+    end
+
+    def end_date
+        end_meeting = start_date + (duration * 60)
+    end
+
+    def is_past?
+        if Time.now > start_date
+            return "Oui"
+        else
+            return "Non"
+        end
+    end
+
+    def is_soon?
+        Time.now.between?(start_date, start_date + 1800)
+    end
+
 end
 
 binding.pry
